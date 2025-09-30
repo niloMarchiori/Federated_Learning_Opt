ef15f59e91627014b29d8a665f82a8a818f00781
Nilo <nilo.marchiori@gmail.com>
2025-09-30 14:40:02 -0300

    node atributes addited

--- opttopology/node.py
+++ opttopology/node.py
@@ -2,10 +2,16 @@ from federated.node import ClientSensor, ServerSensor
 
 
 class SensorOpt(ClientSensor):
-    def __init__(self,cores:int,cpu_freq:list[float,float],tx_power:list[int,int], *args, **kwargs):
+    def __init__(self,cores:str,cpu_freq_curr:float,cpu_freq_min:float,cpu_freq_max:float,tx_power:None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.cores=cores
-        self.cpu_freq=cpu_freq
+        self.cpu_freq_curr=cpu_freq_curr
+        self.cpu_freq_min=cpu_freq_min
+        self.cpu_freq_max=cpu_freq_max
         self.tx_power=tx_power
+        self.data_sz=0
+        self.model_size=0
+
+    
         
 
