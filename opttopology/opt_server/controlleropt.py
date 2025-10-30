d95dfbe0409e10f18ba621ab238b6825c9261a3f
Nilo <nilo.marchiori@gmail.com>
2025-10-30 12:32:38 -0300

    Msg automatica: novos resultados

--- opttopology/opt_server/controlleropt.py
+++ opttopology/opt_server/controlleropt.py
@@ -62,10 +62,10 @@ class Controller:
         self.metrics = {}
 
         self.clients_param={}
-        self.experiment_ctt={'fmin':1.6, #GHz'
-                             'fmax_range': [2.0,3.1], #GHz
+        self.experiment_ctt={'fmin':1.8, #GHz'
+                             'fmax_range': [2.4,3.0], #GHz
                              'alpha': 2E-28,
-                             'kappa': 5,
+                             'kappa': 10**2,
                              'N': self.min_trainers
                              }
         self.model_inputs={}
--- run_experiment.sh
+++ run_experiment.sh
@@ -3,3 +3,4 @@ sudo -E python3 reference_topology/topology.py
 git add .
 git commit -m "Msg automatica: novos resultados"
 git push
+shutdown
