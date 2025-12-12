3b59c6a0c8bcca2f6ea2bcb3e0a7bfe6bdeec553
nilo <nilo.marchiori@gmail.com>
2025-12-12 13:18:58 -0300

    resultados midiid

--- Experiment/energy_padrao/10h41m08senergy_padrao.py
+++ Experiment/energy_padrao/10h41m08senergy_padrao.py
@@ -44,16 +44,17 @@ def main(kappa=100):
     
     #Corresponde a saída do algoritmo apresentado no sbrc
     server_args["output_csv_name"]="metrics_ref_sbrc.csv"
-    # server_args['client_selector']='LeastEnergyConsumption'
-    # topology(server_script,
-    #          client_script, 
-    #          server_args,
-    #          client_args,
-    #          model_inputs,
-    #          cpu_governor='ondemand',
-    #          experiment_name=experiment_name,
-    #          n_rounds=NUM_ROUNDS)
+    server_args['client_selector']='LeastEnergyConsumption'
+    topology(server_script,
+             client_script, 
+             server_args,
+             client_args,
+             model_inputs,
+             cpu_governor='ondemand',
+             experiment_name=experiment_name,
+             n_rounds=NUM_ROUNDS)
     
     
+
 if __name__=='__main__':
     main()
